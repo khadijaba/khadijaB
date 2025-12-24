5b45fe5c23bdd959d97f452ce29bcc2d8fa20116
khadijaba <131060906+khadijaba@users.noreply.github.com>
2025-12-24 21:36:39 +0100

    Update SecurityConfig.java

--- src/main/java/tn/esprit/tpprojet2025/SecurityConfig.java
+++ src/main/java/tn/esprit/tpprojet2025/SecurityConfig.java
@@ -11,11 +11,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .requestMatchers("/tpProjet/actuator/prometheus").permitAll() // autorise Prometheus
-                .anyRequest().authenticated() // le reste reste sécurisé
+                .requestMatchers("/tpProjet/actuator/prometheus").permitAll() // autoriser Prometheus
+                .anyRequest().authenticated() // le reste sécurisé
             .and()
-            .httpBasic().disable()
-            .formLogin().disable()
-            .csrf().disable(); // désactive CSRF pour Actuator
+            .csrf().disable()
+            .formLogin().disable();
     }
 }
